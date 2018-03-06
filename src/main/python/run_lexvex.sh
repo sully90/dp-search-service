76a87d906aab69d749cc0fd426947c3da4b6db79
David Sullivan <dsullivan0190@gmail.com>
2018-03-06 13:26:24 +0000

    Good config for lexvex

--- src/main/python/run_lexvex.sh
+++ src/main/python/run_lexvex.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 export LEXVEX_ROOT="/Users/sullid/golang/src/github.com/alexandres/lexvec"
+MIN_FREQ=15
 
-lexvec -corpus ~/ONS/dp-search-service/src/main/python/ons_corpus.txt -output models/ons_lv.vec -dim 300 -window 5 -subsample 1e-5 -negative 5 -iterations 5 -minfreq 10 -matrix ppmi -model 0
+lexvec -corpus ~/ONS/dp-search-service/src/main/python/ons_corpus.txt -output models/ons_lv.vec -dim 300 -window 5 -subsample 1e-5 -negative 5 -iterations 5 -minfreq ${MIN_FREQ} -matrix ppmi -model 0
 
+echo "Merging..."
 python ${LEXVEX_ROOT}/merge_context_vectors.py models/ons_lv.vec
